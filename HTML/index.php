8caa5673ce6b7d9c197e85abd0e063ec4e3975cb
Pentrax <e.vodilka@gmail.com>
2022-11-21 12:02:50 +0100

    semkaBezPHPFinal

---
+++ HTML/index.php
@@ -0,0 +1,34 @@
+<?php
+    include_once "header.php"
+?>
+
+<div class="domov">
+    <h1>"Moderná multiplayerová hra"</h1>
+    <p>Lorem Ipsum je fiktívny text, používaný pri návrhu tlačovín a typografie. Lorem Ipsum je štandardným
+        výplňovým textom už od 16. storočia,<br> keď neznámy tlačiar zobral sadzobnicu plnú tlačových znakov a
+        pomiešal
+        ich, aby tak vytvoril vzorkovú knihu.<br> Prežil nielen päť storočí, ale aj skok do elektronickej sadzby, a
+        pritom zostal v podstate nezmenený.<br> Spopularizovaný bol v 60-tych rokoch 20.storočia, vydaním hárkov
+        Letraset, ktoré obsahovali pasáže Lorem Ipsum, a neskôr aj publikačným <br>softvérom ako Aldus PageMaker,
+        ktorý
+        obsahoval verzie Lorem Ipsum.</p>
+    <p> - Junior Cesar Javes II.</p>
+</div>
+
+<div class="tlacidloSteam">
+    <a href="https://store.steampowered.com/app/570/Dota_2/">
+        <div class="homepage_PlayButton_2yA33">
+            <div class="homepage_SteamLogo_2OrkR"
+                 style="background-image: url('../images/steam.svg')"></div>
+            <div class="homepage_RightSide_1oC4f">
+                <div class="homepage_PlayForFree_QtAu0">Hrajte Zadarmo</div>
+                <div class="homepage_Download_T3dgN">Stiahnite na Steame</div>
+            </div>
+        </div>
+    </a>
+</div>
+</div>
+</body>
+<footer></footer>
+<script src="../JS/main.js"></script>
+</html>
